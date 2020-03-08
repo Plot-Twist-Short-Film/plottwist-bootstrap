1e37c25f26b359495fa540de9607cca1a7e86808
Tomi Poveda <tpovedatd@gmail.com>
2020-03-08 18:46:06 +0100

    Updated PlotTwist Maya bootstrap

--- plottwist/bootstrap/maya/userSetup.py
+++ plottwist/bootstrap/maya/userSetup.py
@@ -13,20 +13,21 @@ __maintainer__ = "Tomas Poveda"
 __email__ = "tpovedatd@gmail.com"
 
 print('=' * 100)
-print('| Plot Twist Pipeline | > Loading Plot Twist Tools')
+print('| Plot Twist ArtellaPipe | > Loading Plot Twist Tools')
 
 try:
     import plottwist.loader
     from maya import cmds
     cmds.evalDeferred('plottwist.loader.init(import_libs=True)')
-    print('| Plto Twist Pipeline | Plot Twist Tools loaded successfully!')
+    print('| Plto Twist ArtellaPipe | Plot Twist loaded successfully!')
     print('=' * 100)
 except Exception as e:
     try:
         import plottwist.loader
         plottwist.loader.init(import_libs=True)
-        print('| Plot Twist Pipeline | Plot Twist Tools loaded successfully!')
+        print('| Plot Twist Pipeline | Plot Twist loaded successfully!')
         print('=' * 100)
-    except Exception as e:
+    except Exception as exc:
+        import traceback
         print('ERROR: Impossible to load Plot Twist Tools, contact TD!')
-        print(str(e))
+        print('{} | {}'.format(exc, traceback.format_exc()))
